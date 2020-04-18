8957748071e47e6331550c881cffcf3159853656
John Zonneveld <jjczon@gmail.com>
2020-04-18 15:42:52 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class Doctor
+  
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date,patient,self)
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment|
+        appointment.doctor == self
+     end
+   end
+   
+   def patients
+     Appointment.all.map do |appointment|
+      # binding.pry
+       appointment.patient
+     end
+   end
+    
+  
+  def self.all
+    @@all
+  end
+  
+end
