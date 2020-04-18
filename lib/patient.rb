d0676b5f994745f574fa08ee7588ed71cae5d325
John Zonneveld <jjczon@gmail.com>
2020-04-18 15:37:54 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,6 +13,11 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+    
+  end
+  
   def self.all
     @@all
   end
