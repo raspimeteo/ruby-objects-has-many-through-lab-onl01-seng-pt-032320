14a4a5823328c2c2836cd88e77710f01bdb25ac0
John Zonneveld <jjczon@gmail.com>
2020-04-18 15:24:40 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,22 @@
+class Appointment
+  
+  attr_reader :doctor, :patient
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def patient
+    Appointment.all.select {|appointment| appointment.patient}
+  end
+  
+end
